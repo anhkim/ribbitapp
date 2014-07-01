fee4cac4d8639b746e0372b5c19775e0f68a3406
anhkim <akhoang88@gmail.com>
2014-06-30 20:13:09 -0700

    app controllers bug removed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_username params[:username]
-    if user and user.authenicate(params[:password])
+    if user and user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "You are logged in."
     else
